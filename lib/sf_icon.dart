fc880818fc32e8b3dec26919351bed5ec4c5b940
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2023-07-22 21:00:08 +0530

    `SfIcon` has been rewritten. The old implementation is available as `SfIconOld`.
    This has been done because this makes golden testing slightly easier and causes almost no change in behavior.
    
    Additional test-only arguments have been added to both classes.

--- lib/sf_icon.dart
+++ lib/sf_icon.dart
@@ -12,6 +12,12 @@ class SfIcon extends StatelessWidget {
     this.iconWeight,
     this.iconOpticalSize,
     this.iconGrade,
+    this.fontStyle,
+    this.leadingDistribution,
+    this.baseline,
+    this.letterSpacing,
+    this.height,
+    this.isTest = false,
   });
 
   final double? iconFill;
@@ -28,6 +34,96 @@ class SfIcon extends StatelessWidget {
 
   final double? size;
 
+  final FontStyle? fontStyle;
+
+  /// Only exposed for use in golden tests, DO NOT use otherwise unless necessary
+  final TextLeadingDistribution? leadingDistribution;
+
+  /// Only exposed for use in golden tests, DO NOT use otherwise unless necessary
+  final TextBaseline? baseline;
+
+  /// Only exposed for use in golden tests, DO NOT use otherwise unless necessary
+  final double? letterSpacing;
+
+  /// Only exposed for use in golden tests, DO NOT use otherwise unless necessary
+  final double? height;
+
+  /// Only exposed for use in golden tests, DO NOT use otherwise unless necessary
+  final bool isTest;
+
+  @override
+  Widget build(BuildContext context) {
+    IconThemeData iconThemeData = IconTheme.of(context);
+
+    return Text(
+      String.fromCharCode(iconData.codePoint),
+      style: TextStyle(
+        inherit: false,
+        fontFamily: iconData.fontFamily,
+        package: isTest ? null : iconData.fontPackage,
+        color: iconColor ?? iconThemeData.color,
+        fontSize: size ?? iconThemeData.size,
+        fontStyle: fontStyle,
+        fontVariations: <FontVariation>[
+          if (iconFill != null)
+            FontVariation(
+              'FILL',
+              iconFill ?? iconThemeData.fill ?? 0.0,
+            ),
+          if (iconWeight != null)
+            FontVariation(
+              'wght',
+              iconWeight ?? iconThemeData.weight ?? 400,
+            ),
+          if (iconGrade != null)
+            FontVariation(
+              'GRAD',
+              iconGrade ?? iconThemeData.grade ?? 0.0,
+            ),
+          if (iconOpticalSize != null)
+            FontVariation(
+              'opsz',
+              iconOpticalSize ?? iconThemeData.opticalSize ?? 48,
+            ),
+        ],
+        height: height,
+        leadingDistribution: leadingDistribution,
+        textBaseline: baseline,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
+}
+
+class SfIcon1 extends StatelessWidget {
+  const SfIcon1(
+    this.iconData, {
+    super.key,
+    this.iconColor,
+    this.size,
+    this.iconFill,
+    this.iconWeight,
+    this.iconOpticalSize,
+    this.iconGrade,
+    this.fontStyle,
+  });
+
+  final double? iconFill;
+
+  final double? iconWeight;
+
+  final double? iconOpticalSize;
+
+  final double? iconGrade;
+
+  final IconData iconData;
+
+  final Color? iconColor;
+
+  final double? size;
+
+  final FontStyle? fontStyle;
+
   @override
   Widget build(BuildContext context) {
     IconThemeData iconThemeData = IconTheme.of(context);
@@ -38,6 +134,7 @@ class SfIcon extends StatelessWidget {
       text: TextSpan(
         text: String.fromCharCode(iconData.codePoint),
         style: TextStyle(
+          fontStyle: fontStyle,
           fontVariations: <FontVariation>[
             if (iconFill != null)
               FontVariation(
